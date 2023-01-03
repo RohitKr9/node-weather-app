690990b3bea079ef65f520b48eb0fff0c6d0c777
rohit kumar <rohitpoddar937@gmail.com>
2023-01-03 19:20:23 +0530

    fun changes

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ const weatherForm=document.querySelector('form')
 const Search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
 const messageTwo=document.querySelector('#message-2')
+const messageThree=document.querySelector('#message-3')
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
@@ -11,6 +12,7 @@ weatherForm.addEventListener('submit',(e)=>{
 
     messageOne.textContent='loading...'
     messageTwo.textContent=''
+    messageThree.textContent=''
 
     fetch('/weather?location='+location).then((response)=>{
     response.json().then((data=>{
@@ -19,6 +21,11 @@ weatherForm.addEventListener('submit',(e)=>{
         }else{
             messageOne.textContent=data.address
             messageTwo.textContent=data.temp+' degree'
+            var place=data.address
+            if(place.includes('Nathnagar')||place.includes('Bhagalpur')){
+                messageThree.textContent='Amarpur bhi search kar lezeye bajoria ji😁:))'
+            }
+            
         }
         console.log(data.error)
     }))
